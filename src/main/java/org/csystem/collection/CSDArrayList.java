208da9f50ba1936c779f2858320b6559d0fc4218
m-gunes <mistafagunes@gmail.com>
2026-04-30 11:40:53 +0200

    added CSDArrayList

---
+++ src/main/java/org/csystem/collection/CSDArrayList.java
@@ -0,0 +1,304 @@
+package org.csystem.collection;
+
+import java.util.*;
+
+public class CSDArrayList<E> implements List<E> {
+    private static final String TO_STRING_SEPARATOR = ", ";
+    private static final String TO_STRING_PREFIX = "[";
+    private static final String TO_STRING_SUFFIX = "]";
+    private static final int DEFAULT_CAPACITY = 10;
+    private E [] m_elements;
+    private int m_index;
+
+    private void throwIllegalArgumentException(String message)
+    {
+        throw new IllegalArgumentException(message);
+    }
+
+    private void throwIndexOutOfBoundsException(String message)
+    {
+        throw new IndexOutOfBoundsException(message);
+    }
+
+    private void checkCapacity(int capacity)
+    {
+        if (capacity < 0)
+            throwIllegalArgumentException(String.format("Capacity must be non-negative:%d", capacity));
+    }
+
+    private void changeCapacity(int capacity)
+    {
+        m_elements = Arrays.copyOf(m_elements, capacity);
+    }
+
+    private void checkIndex(int index)
+    {
+        if (index < 0 || index > m_index)
+            throwIndexOutOfBoundsException(String.format("Index out of bounds:%d", index));
+    }
+
+    private void increaseCapacityIfNecessary()
+    {
+        if (m_elements.length == m_index)
+            changeCapacity(m_elements.length == 0 ? 1 : m_elements.length * 2);
+    }
+
+    @SuppressWarnings("unchecked")
+    public CSDArrayList()
+    {
+        m_elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    @SuppressWarnings("unchecked")
+    public CSDArrayList(int initialCapacity)
+    {
+        checkCapacity(initialCapacity);
+        m_elements = (E[]) new Object[initialCapacity];
+    }
+
+    public CSDArrayList(Collection<? extends E> c)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    @Override
+    public boolean add(E element)
+    {
+        increaseCapacityIfNecessary();
+        m_elements[m_index++] = element;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void add(int index, E element)
+    {
+        increaseCapacityIfNecessary();
+
+        for (int i = m_index++; i > index; --i)
+            m_elements[i] = m_elements[i - 1];
+
+        m_elements[index] = element;
+    }
+
+    public int capacity()
+    {
+        return m_elements.length;
+    }
+
+    @Override
+    public void clear()
+    {
+        for (int i = 0; i < m_index; ++i)
+            m_elements[i] = null;
+
+        m_index = 0;
+    }
+
+    public void ensureCapacity(int minCapacity)
+    {
+        if (minCapacity > m_elements.length)
+            changeCapacity(Math.max(minCapacity, m_elements.length * 2));
+    }
+
+    @Override
+    public E get(int index)
+    {
+        checkIndex(index);
+        return m_elements[index];
+    }
+
+    @Override
+    public E remove(int index)
+    {
+        checkIndex(index);
+        E oldElement = m_elements[index];
+
+        for (int i = index; i < m_index - 1; ++i)
+            m_elements[i] = m_elements[i+1];
+
+        m_elements[--m_index] = null;
+
+        return oldElement;
+    }
+
+    @Override
+    public int indexOf(Object o)
+    {
+        if (o == null) {
+            for (int i = 0; i < m_index; ++i)
+                if (m_elements[i] == null)
+                    return i;
+        }
+        else {
+            for (int i = 0; i < m_index; ++i)
+                if (o.equals(m_elements[i]))
+                    return i;
+        }
+
+//        for (int i = 0; i < m_index; ++i)
+//            if (Objects.equals(o, m_elements[i]))
+//                return i;
+
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    @Override
+    public ListIterator<E> listIterator()
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public E set(int index, E element)
+    {
+        checkIndex(index);
+        E oldElement = m_elements[index];
+        m_elements[index] = element;
+        return oldElement;
+    }
+
+    @Override
+    public int size()
+    {
+        return m_index;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    @Override
+    public boolean contains(Object o)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    @Override
+    public Iterator<E> iterator()
+    {
+        return new Iterator<E>() {
+            int idx;
+
+            @Override
+            public boolean hasNext()
+            {
+                return idx < m_index;
+            }
+
+            @Override
+            public E next()
+            {
+                if (!hasNext())
+                    throw new NoSuchElementException("No more item!");
+                else {
+                    return m_elements[idx++];
+                }
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray()
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void trimToSize()
+    {
+        if (m_elements.length != m_index)
+            changeCapacity(m_index);
+    }
+
+    @Override
+    public boolean equals(Object other)
+    {
+        if (other == this)
+            return true;
+
+        if (!(other instanceof CSDArrayList<?> otherlist))
+            return false;
+
+        if (otherlist.size() != m_index)
+            return false;
+
+        for (var i = 0; i < m_index; ++i)
+            if (!Objects.equals(m_elements[i], otherlist.m_elements[i]))
+                return false;
+
+        return true;
+    }
+
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder(TO_STRING_PREFIX);
+        for (int i = 0; i < m_index; ++i)
+            sb.append(m_elements[i]).append(TO_STRING_SEPARATOR);
+
+        return (m_index != 0 ? sb.substring(0, sb.length() - TO_STRING_SEPARATOR.length()) : sb.toString()) + TO_STRING_SUFFIX;
+    }
+
+
+
+}
