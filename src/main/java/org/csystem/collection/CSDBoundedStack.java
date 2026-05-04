d3078009b934ad32f4baf8899ac996793d1e33d4
m-gunes <mistafagunes@gmail.com>
2026-05-04 14:39:47 +0200

    homework CSDBoundedStack

---
+++ src/main/java/org/csystem/collection/CSDBoundedStack.java
@@ -0,0 +1,40 @@
+package org.csystem.collection;
+
+/**
+ * Sınıf Çalışması: <p>
+ *   Eleman sayısını ctor ile alan ve stack dolduğunda RuntimeException sınıfından türetilmiş FullStackException fırlatan aşağıdaki CSDBoundedStack sınıfını yazınız.
+ *
+ *   <p>
+ *   Açıklamalar:
+ *   <p>
+ *     Sınıfın public bölümünü değiştirmeden istediğiniz eklemeyi yapabilirsiniz.
+ */
+// iceride dizi veri yapisi kullan.
+// Size doldugunda FullStackException firlat
+
+public class CSDBoundedStack<E> {
+    public CSDBoundedStack(int size)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public E peek()
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public E pop()
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public E push(E item)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public int search(E item)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+}
---
+++ src/main/java/org/csystem/collection/exception/FullStackException.java
@@ -0,0 +1,8 @@
+package org.csystem.collection.exception;
+
+import java.util.EmptyStackException;
+
+//    EmptyStackException'a bakarak bunu tasarlayabilirsiniz
+public class FullStackException extends RuntimeException {
+    //TODO
+}
