ebb9644450163332d534f3bb4cc901679a496193
m-gunes <mistafagunes@gmail.com>
2026-05-04 14:29:29 +0200

    homework CSDStack

---
+++ src/main/java/org/csystem/collection/CSDStack.java
@@ -0,0 +1,39 @@
+package org.csystem.collection;
+
+/**
+ * Sınıf Çalışması: <p>
+ * Yalnızca Object sınıfından türetilen ve dinamik olarak büyüyebilen aşağıdaki CSDStack sınıfını yazınız.
+ *
+ * <p>
+ * Açıklamalar: <p>
+ *
+ * <p>Sınıfın public bölümünü değiştirmeden istediğiniz eklemeyi yapabilirsiniz
+ * <p>JavaSE'nin Stack<E> sınıfı kullanılmayacaktır.
+ */
+// herhangi bir collection siniftan turemeyecek ama iceride kullanabilirsiniz. stack disinda
+public class CSDStack<E> {
+    public CSDStack()
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public E peek()
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public E pop()
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public E push(E item)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public int search(E item)
+    {
+        throw new UnsupportedOperationException("TODO");
+    }
+}
