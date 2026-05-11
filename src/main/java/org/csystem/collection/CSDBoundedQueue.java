99ab8fbaa3c61ba15f97329097fc1d159dd62626
m-gunes <mistafagunes@gmail.com>
2026-05-11 18:04:39 +0200

    add queue, linkedList examples

---
+++ src/main/java/org/csystem/collection/CSDBoundedQueue.java
@@ -0,0 +1,7 @@
+package org.csystem.collection;
+
+/**
+ * Sınıf Çalışması: Eleman sayısını ctor ile alan ve queue dolduğunda RuntimeException sınıfından türetilmiş FullQueueException fırlatan CSDBoundedQueue sınıfını yazınız ve test ediniz.
+ */
+public class CSDBoundedQueue {
+}
---
+++ src/main/java/org/csystem/collection/CSDQueue.java
@@ -0,0 +1,11 @@
+package org.csystem.collection;
+
+import java.util.Queue;
+
+/**
+ * Sınıf Çalışması: Yalnızca Object sınıfından türetilen, Queue<E> arayüzünü implemente eden CSDQueue sınıfını dinamik büyüyen dizi implementasyonu olarak yazınız ve test ediniz.
+ * @param <E>
+ */
+
+public class CSDQueue<E> {
+}
